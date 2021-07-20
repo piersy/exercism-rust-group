7081315a67b17b181be68d69c5df203c403b3ecb
Piers Powlesland <pierspowlesland@gmail.com>
2021-07-20 15:42:32 +0100

    Complete clock exercise

--- piersy/rust/clock/src/lib.rs
+++ piersy/rust/clock/src/lib.rs
@@ -6,14 +6,15 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let extra_hours = minutes / 60;
-        let mut hours = (hours + extra_hours) % 24;
-        if hours < 0 {
-            hours = hours + 24;
-        }
+        let mut extra_hours = minutes / 60;
         let mut minutes = minutes % 60;
         if minutes < 0 {
             minutes = minutes + 60;
+            extra_hours = extra_hours - 1;
+        }
+        let mut hours = (hours + extra_hours) % 24;
+        if hours < 0 {
+            hours = hours + 24;
         }
 
         Clock {
@@ -23,13 +24,7 @@ impl Clock {
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let extra_hours = minutes / 60;
-        Clock {
-            hours: self.hours + extra_hours,
-            minutes: minutes % 60,
-        }
-
-        // unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock::new(self.hours, self.minutes + minutes)
     }
 }
 
@@ -38,9 +33,3 @@ impl ToString for Clock {
         format!("{:02}:{:02}", self.hours, self.minutes)
     }
 }
-
-// impl fmt::Display for Clock {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         write!(f, "({}, {})", self.hours, self.minutes)
-//     }
-// }
