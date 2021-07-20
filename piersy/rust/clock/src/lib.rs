cd35a0d8febd8a7d57c3546e17328bca6bcbd655
Piers Powlesland <pierspowlesland@gmail.com>
2021-07-20 15:15:05 +0100

    Fixed first clock problem

--- piersy/rust/clock/src/lib.rs
+++ piersy/rust/clock/src/lib.rs
@@ -1,15 +1,38 @@
-pub struct Clock;
+#[derive(Eq, PartialEq, Debug)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let extra_hours = minutes / 60;
+        let hours = hours + extra_hours;
+        Clock {
+            hours: hours % 24,
+            minutes: minutes % 60,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        let extra_hours = minutes / 60;
+        Clock {
+            hours: self.hours + extra_hours,
+            minutes: minutes % 60,
+        }
+
+        // unimplemented!("Add {} minutes to existing Clock time", minutes);
     }
 }
+
+impl ToString for Clock {
+    fn to_string(&self) -> String {
+        format!("{:02}:{:02}", self.hours, self.minutes)
+    }
+}
+
+// impl fmt::Display for Clock {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "({}, {})", self.hours, self.minutes)
+//     }
+// }
