d380a7a0b2fbfd2ad668e25b8834972c6da7bfaa
Piers Powlesland <pierspowlesland@gmail.com>
2021-07-20 12:50:07 +0100

    Most reverse string tests passing

--- piersy/rust/reverse-string/src/lib.rs
+++ piersy/rust/reverse-string/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {}", input);
+    // print!(""");
+    let mut s = String::with_capacity(input.len());
+
+    for c in input.chars().rev() {
+        s.push(c);
+    }
+    return s;
 }
