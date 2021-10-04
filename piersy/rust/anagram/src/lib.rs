6dffd27d54b92604168ce8c4ccc229190a0e2501
Piers Powlesland <pierspowlesland@gmail.com>
2021-10-04 21:59:51 +0100

    piersy: Fix anagram code account for self anagram

--- piersy/rust/anagram/src/lib.rs
+++ piersy/rust/anagram/src/lib.rs
@@ -1,52 +1,49 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::option;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     // This will find anagrams by inserting the word characters into a haset and then checking that
     // they are matched by the possible anagrams.
 
     let mut m = HashMap::new();
     let mut result: HashSet<&'a str> = HashSet::new();
 
+    let l_word = word.to_lowercase();
     // Add lowercased word chars and count to hashmap.
-    for c in word.chars() {
-        for l in c.to_lowercase() {
-            let count = m.entry(l).or_insert(0);
-            *count += 1;
-        }
+    for c in l_word.chars() {
+        let count = m.entry(c).or_insert(0);
+        *count += 1;
     }
-    for w in possible_anagrams {
-        let mc = m.clone();
+    for a in possible_anagrams {
+        let l_a = a.to_lowercase();
+        if l_a == l_word {
+            continue;
+        }
+        let mut mc = m.clone();
+        if is_anagram(&mut mc, &l_a) {
+            result.insert(a);
+        }
     }
     result
 }
 
-fn isAnagram(char_map: &mut HashMap<char, i32>, word: &str) -> bool {
+fn is_anagram(char_map: &mut HashMap<char, i32>, word: &str) -> bool {
     for c in word.chars() {
-        for l in c.to_lowercase() {
-            let mut failed = false;
-            let o = char_map.get_mut(&l);
-            match o {
-                None => failed = true,
-                Some(v) => {
-                    *v -= 1;
-                    if *v < 0 {
-                        failed = true;
-                    }
+        let o = char_map.get_mut(&c);
+        match o {
+            None => return false,
+            Some(v) => {
+                *v -= 1;
+                if *v < 0 {
+                    return false;
                 }
             }
         }
     }
+    for x in char_map.values() {
+        if *x > 0 {
+            return false;
+        }
+    }
     true
-}
-
-fn foo(param: bool) -> i32 {
-    if param {
-        return 5;
-    };
-
-    println!("param is: {:?}", param);
-
-    6
 }
