d608fcf05b22535895ef4ab4f4ad9697e49bc5a1
Piers Powlesland <pierspowlesland@gmail.com>
2021-10-06 18:28:07 +0100

    Getting close to working

--- piersy/rust/dot-dsl/src/lib.rs
+++ piersy/rust/dot-dsl/src/lib.rs
@@ -1,18 +1,5 @@
 pub mod graph {
     use std::collections::HashMap;
-    pub trait Attributable<'a> {
-        fn with_attrs(&mut self, attrs: &[(&'a str, &'a str)]) -> Self {
-            // self.attrs = HashMap::new();
-            let m = self.get_attrs();
-            for (k, v) in attrs {
-                // self.attrs.insert(k, v);
-                m.insert(k, v);
-            }
-            *self
-        }
-
-        fn get_attrs(&mut self) -> &mut HashMap<&'a str, &'a str>;
-    }
     pub struct Graph<'a> {
         pub nodes: Vec<graph_items::node::Node<'a>>,
         pub edges: Vec<graph_items::edge::Edge<'a>>,
@@ -36,18 +23,27 @@ pub mod graph {
             self.edges = edges.to_vec();
             self
         }
-    }
-
-    impl<'a> Attributable<'a> for Graph<'a> {
-        fn get_attrs(&mut self) -> &mut HashMap<&'a str, &'a str> {
-            &mut self.attrs
+        pub fn with_attrs(mut self, attrs: &[(&'a str, &'a str)]) -> Self {
+            for (k, v) in attrs {
+                self.attrs.insert(k, v);
+            }
+            self
+        }
+        pub fn get_node(&self, name: &str) -> Option<&'a graph_items::node::Node> {
+            for n in &self.nodes {
+                if n.v == name {
+                    return Some(n);
+                }
+            }
+            None
         }
     }
 
     pub mod graph_items {
         pub mod edge {
             use std::collections::HashMap;
-            #[derive(Clone)]
+            // #[derive(Clone)]
+            #[derive(Clone, Eq, PartialEq, Debug)]
             pub struct Edge<'a> {
                 start: &'a str,
                 end: &'a str,
@@ -62,17 +58,17 @@ pub mod graph {
                         attrs: HashMap::new(),
                     }
                 }
-            }
-
-            impl<'a> super::super::Attributable<'a> for Edge<'a> {
-                fn get_attrs(&mut self) -> &mut HashMap<&'a str, &'a str> {
-                    &mut self.attrs
+                pub fn with_attrs(mut self, attrs: &[(&'a str, &'a str)]) -> Self {
+                    for (k, v) in attrs {
+                        self.attrs.insert(k, v);
+                    }
+                    self
                 }
             }
         }
         pub mod node {
             use std::collections::HashMap;
-            #[derive(Clone)]
+            #[derive(Clone, Eq, PartialEq, Debug)]
             pub struct Node<'a> {
                 pub v: &'a str,
                 pub attrs: HashMap<&'a str, &'a str>,
@@ -84,10 +80,14 @@ pub mod graph {
                         attrs: HashMap::new(),
                     }
                 }
-            }
-            impl<'a> super::super::Attributable<'a> for Node<'a> {
-                fn get_attrs(&mut self) -> &mut HashMap<&'a str, &'a str> {
-                    &mut self.attrs
+                pub fn with_attrs(mut self, attrs: &[(&'a str, &'a str)]) -> Self {
+                    for (k, v) in attrs {
+                        self.attrs.insert(k, v);
+                    }
+                    self
+                }
+                pub fn get_attr(&self, name: &str) -> Option<&&'a str> {
+                    self.attrs.get(name)
                 }
             }
         }
--- piersy/rust/dot-dsl/tests/dot-dsl.rs
+++ piersy/rust/dot-dsl/tests/dot-dsl.rs
@@ -1,6 +1,5 @@
 use dot_dsl::graph::graph_items::edge::Edge;
 use dot_dsl::graph::graph_items::node::Node;
-use dot_dsl::graph::Attributable;
 use dot_dsl::graph::Graph;
 use maplit::hashmap;
 
