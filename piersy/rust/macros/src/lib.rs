90ddac8144864927d03160d2a4135003e41393c3
Piers Powlesland <pierspowlesland@gmail.com>
2021-11-09 16:44:31 +0000

    piersy: Fix case of single comma
    The previous matcher would match just a single comma.

--- piersy/rust/macros/src/lib.rs
+++ piersy/rust/macros/src/lib.rs
@@ -3,7 +3,14 @@
 
 #[macro_export]
 macro_rules! hashmap {
-    ( $($k:expr => $v:expr),* $(,)?) => {{
+    ( $($k:expr => $v:expr),*) => {{
+        let mut temp_map = ::std::collections::HashMap::new();
+        $(
+            temp_map.insert($k,$v);
+        )*
+        temp_map
+    }};
+    ( $($k:expr => $v:expr),+ ,) => {{
         let mut temp_map = ::std::collections::HashMap::new();
         $(
             temp_map.insert($k,$v);
